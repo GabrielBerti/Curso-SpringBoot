b57ccb9adc44ed3ca107b76e3a231c3a1d69a223
GabrielBerti <gabriel_desanti@hotmail.com>
2021-02-05 19:47:03 -0300

    xxx

--- src/main/java/com/eventoapp/DataConfiguration.java
+++ src/main/java/com/eventoapp/DataConfiguration.java
@@ -33,6 +33,29 @@ public class DataConfiguration {
         return basicDataSource;
     }
 	
+	// tst
+	/*@Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        //dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?autoReconnect=true&useSSL=false");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/eventosapp?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("Gabriel123321");
+        return dataSource;
+    }*/
 	
-	
+	/*@Bean
+	public JpaVendorAdapter jpaVendorAdapter(){
+		HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+		adapter.setDatabase(Database.MYSQL);
+		adapter.setShowSql(true);
+		adapter.setGenerateDdl(true);
+		//adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+		adapter.setDatabasePlatform("org.hibernate.dialect.MySQL8Dialect");
+		adapter.setPrepareConnection(true);
+				
+		return adapter;
+	}*/
+
 }
